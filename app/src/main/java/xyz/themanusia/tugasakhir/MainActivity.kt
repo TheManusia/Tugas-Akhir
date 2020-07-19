764851387f42fc7ef5ff53688d3db7f9a4dfceff
Ian <minecraftindo0@gmail.com>
2020-07-20 00:00:55 +0700

    Initialize Project

---
+++ app/src/main/java/xyz/themanusia/tugasakhir/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.themanusia.tugasakhir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
